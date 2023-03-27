e8db6dd564a84e21af3f397273efbacbc5fa84c1
sandesh bafna <sandesh.bafna8@gmail.com>
2023-03-27 20:12:15 +0530

    Make https as default protocol for host and use darker color for heat map blocks in analytics (#40)
    * https default protocol and darkert heat map boxes
    
    * https default protocol and darkert heat map boxes
    
    * Optimise host rectfier function
    
    * Use indigo 300 - 800 for Heatmap
    
    * Revert accidental changes
    
    ---------
    
    Co-authored-by: sbafna1 <sbafna@ip-10-170-33-212>

--- web/src/pages/Analytics.tsx
+++ web/src/pages/Analytics.tsx
@@ -84,7 +84,7 @@ const NotificationZoneMap = () => {
               <Progress
                 colorScheme="transparent"
                 height={2}
-                bgGradient="linear(to-r, rgba(79, 52, 199, 1) 0%, rgba(232, 230, 245) 100%)"
+                bgGradient="linear(to-r, #3A249E 0%, #CCC3F9 100%)"
                 value={90}
               />
             </Box>
@@ -103,7 +103,7 @@ const NotificationZoneMap = () => {
         <Heatmap
           height={400}
           useCells={useConversionCells}
-          colorInterpolater={interpolateRgb("#E8E6F5", "#4F34C7")}
+          colorInterpolater={interpolateRgb("#CCC3F9", "#3A249E")}
           getCellConfig={({ conversionRate, wktPolygon }: ZoneMetrics) => ({
             value: conversionRate,
             wktPolygon,
